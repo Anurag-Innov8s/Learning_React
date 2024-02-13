83963ceed4cbfd72b026110050310ef5906a5a6f
Anurag Kumar <117522119+Anurag-Innov8s@users.noreply.github.com>
2024-02-14 02:03:35 +0530

    Update Welcome.jsx

--- src/Components/Welcome/Welcome.jsx
+++ src/Components/Welcome/Welcome.jsx
@@ -9,9 +9,6 @@ const Welcome = () => {
         <h1>Welcome to the Learning Portal</h1>
       </div>
       <div className='picture'>
-        <img src="https://imgs.search.brave.com/xtaN_GlV-UzpU5kpylGHpQIsh6142wdCd8Q_Sb5KmXM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by9jcmVhdGl2ZS1i/aWctZGF0YS1jaGFy/dHMtd2FsbHBhcGVy/XzY3MDE0Ny05MDcx/LmpwZz9zaXplPTYy/NiZleHQ9anBn" alt="Description of the image" />
-      </div>
-      <div className='picture'>
         <img src="https://imgs.search.brave.com/pN6ExBMCVf2HoUI7fH-GyZnQU--bL6Y82VylIpK5dmM/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJjYXZlLmNv/bS93cC93cDQ3NDg0/NzYuanBn" alt="Image Description" />
       </div>
       <div className='picture'>
